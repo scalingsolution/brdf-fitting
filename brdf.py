slopes and normalizing fixed

brdf.py
@@ -1,19 +1,22 @@
 import enoki as ek
 import numpy as np
 
-"""
-Build kernel matrix for calculation of micro-facet Normal Distribution
-Function
+# Minimal divison coefficient
+EPSILON = 1e-4
 
+
+"""
+Build kernel matrix for calculation of micro-facet
+Normal Distribution Function (NDF).
 params:
     @frC = cosign weighted BSDF mesurements
     @n_theta = elevation samples of input
     @n_phi = azimuth samples of input / calculation
     @isotropic = material property isotropic
 return:
-    @K = kernel matrix
+    @K = NDF kernel matrix
 """
-def build_kernel(frC, n_theta, n_phi, isotropic):
+def build_ndf_kernel(frC, n_theta, n_phi, isotropic):
     N = frC.size
     # Build kernel matrix
     K = np.zeros((N,N))
@@ -23,36 +26,144 @@ def build_kernel(frC, n_theta, n_phi, isotropic):
             raise("Error: Grid dimensions do not match BRDF mesurements!")
         from scipy.integrate import trapz
         # Interpolation grid
-        theta_m = u2theta(np.linspace(0, 1, n_theta))
-        phi_m = u2phi(np.linspace(0, 1, n_phi))
-        dphi = phi_m[1] - phi_m[0]      # constant phi increment
+        theta = u2theta(np.linspace(0, 1, n_theta))
+        phi = u2phi(np.linspace(0, 1, n_phi))
+        dphi = 2 * np.pi / (n_phi - 1)      # constant increments (= phi[1]-phi[0])
 
         # Directions of resulting kernel (only dependant on theta)
         # phi = 0 (arbitrary choice)
-        omega = np.column_stack((np.sin(theta_m),
+        omega = np.column_stack((np.sin(theta),
                                  np.zeros(n_theta),
-                                 np.cos(theta_m)))
+                                 np.cos(theta)))
         for j in range(N):
             # Integrate over phi_m [-pi, pi]
-            sin_theta_m, cos_theta_m = (np.sin(theta_m[j]), np.cos(theta_m[j]))
-            sin_phi_m, cos_phi_m = (np.sin(phi_m), np.cos(phi_m))
+            sin_phi_m, cos_phi_m = (np.sin(phi), np.cos(phi))
 
-            omega_m = np.vstack((cos_phi_m * sin_theta_m,
-                                 sin_phi_m * sin_theta_m,
-                                 cos_theta_m * np.ones(n_phi)))
-            tmp = np.matmul(omega, omega_m).clip(0, None) * sin_theta_m
+            omega_m = np.vstack((cos_phi_m * omega[j, 0],
+                                 sin_phi_m * omega[j, 0],
+                                 omega[j, 2] * np.ones(n_phi)))
+            tmp = np.matmul(omega, omega_m).clip(0, None) * omega[j, 0]
             integral = trapz(tmp, dx=dphi, axis=1)
 
             # Calculate kernel
-            K[:, j] = weights * frC * integral
+            K[:, j] = weights[j] * frC * integral
     else:
         if (n_theta * n_phi) != N:
             raise("Error: Grid dimensions do not match BRDF mesurements!")
         _, _, omega_m = grid_sample(n_theta, n_phi)
         for j in range(N):
             # Calculate kernel
-            K[:, j] = weights * frC * np.dot(omega_m, omega_m[j]).clip(0, None)
+            K[:, j] = weights[j] * frC * np.dot(omega_m, omega_m[j]).clip(0, None)
     return K
+
+
+"""
+Build kernel matrix for calculation of slopes of
+micro-facet Normal Distribution Function (NDF).
+Corresponds to the Probability Density Function (PDF).
+params:
+    @frC = cosign weighted BSDF mesurements
+    @n_theta = elevation samples of input
+    @n_phi = azimuth samples of input / calculation
+    @isotropic = material property isotropic
+return:
+    @K = PDF kernel matrix
+"""
+def build_slope_kernel(frC, n_theta, n_phi, isotropic):
+    N = frC.size
+    # Build kernel matrix
+    K = np.zeros((N,N))
+    if isotropic:
+        if n_theta != N:
+            raise("Error: Grid dimensions do not match BRDF mesurements!")
+        from scipy.integrate import trapz
+        # Interpolation grid
+        theta = u2theta(np.linspace(0, 1, n_theta))
+        phi = u2phi(np.linspace(0, 1, n_phi))
+        dphi = 2 * np.pi / (n_phi - 1)      # constant increments (= phi[1]-phi[0])
+        # Directions of resulting kernel (only dependant on theta)
+        # phi = 0 (arbitrary choice)
+        omega = np.column_stack((np.sin(theta),
+                                 np.zeros(n_theta),
+                                 np.cos(theta)))
+        for j in range(N):
+            # Integrate over phi_m [-pi, pi]
+            if omega[j, 2] > EPSILON:
+                sin_phi_m, cos_phi_m = (np.sin(phi), np.cos(phi))
+
+                omega_m = np.vstack((cos_phi_m * omega[j, 0],
+                                     sin_phi_m * omega[j, 0],
+                                     omega[j, 2] * np.ones(n_phi)))
+                tmp = np.matmul(omega, omega_m).clip(0, None)
+                integral = trapz(tmp, dx=dphi, axis=1)
+
+                # Calculate kernel
+                K_ = frC * integral / np.power(omega[j, 2], 4)
+                K[:, j] = K_ * omega[j, 0] * np.power(omega[:,2], 4)
+    else:
+        if (n_theta * n_phi) != N:
+            raise("Error: Grid dimensions do not match BRDF mesurements!")
+        _, phi_m, omega_m = grid_sample(n_theta, n_phi)
+        sin_phi_m = np.sin(phi_m)
+        for j in range(N):
+            if omega_m[j, 2] > EPSILON:
+                # Calculate kernel
+                K_ = (frC * np.dot(omega_m, omega_m[j]).clip(0, None) 
+                      / np.power(omega[j, 2], 4))
+                K[:, j] = K_ * np.power(omega[:,2], 4)           
+    return K
+
+
+
+
+"""
+Normalize NDF slopes.
+Integral over R^2 space of PDF must equal 1.
+Equivalent to integral over hemisphere of
+NDF * cos(theta).
+params:
+    @P_in = NDF slopes
+    @isotropic = material property isotropic
+return:
+    @P = normalized NDF slopes
+"""
+def normalize_slopes(P_in, isotropic):
+    from scipy.integrate import trapz
+    # Get dimensions from slope PDF
+    P = P_in
+    n_theta = P.shape[1]
+    n_phi = P.shape[0]
+    theta = u2theta(np.linspace(0, 1, n_theta))
+    phi = u2theta(np.linspace(0, 1, n_theta))
+    if isotropic:
+        dphi = 2 * np.pi
+        tmp = np.zeros(n_theta)
+        for i in range(n_theta):
+            r = np.tan(theta[i])
+            cos_theta = np.cos(theta[i])
+            if cos_theta > EPSILON:
+                tmp[i] = r * P[0, i] / np.power(cos_theta, 2)
+        integral = trapz(tmp, theta)  # integrate over dtheta
+        integral *= dphi  # integrate over dphi
+    else:
+        dphi = 2 * np.pi / (n_phi - 1)  # constant increments (phi[1]-phi[0])
+        integral = 0.
+        for i in range(n_phi):
+            tmp = np.zeros(n_theta)
+            for j in range(n_theta):
+                r = np.tan(theta[j])
+                cos_theta = np.cos(theta[j])
+                if cos_theta > EPSILON:
+                    tmp[j] = r * P[i, j] / np.power(cos_theta, 2)
+            integral += trapz(tmp, theta)  # integrate over dtheta
+        # TODO: check factor 2
+        integral *= dphi  # integrate over dphi
+    # Normalize PDF
+    assert(integral > 0.)
+    integral = 1. / integral
+    P *= integral
+    return P
+
 
 """
 Compute projected area of micro-facets as nomalisation
@@ -373,15 +484,15 @@ return
 """
 def grid_sample(n_theta, n_phi):
     # Create uniform samples and warp by G2 mapping
-    u_m = np.meshgrid(np.linspace(0, 1, n_theta), np.linspace(0, 1, n_phi))
-    theta_m = u2theta(u_m[0]).flatten()
-    phi_m = u2phi(u_m[1]).flatten()
+    u = np.meshgrid(np.linspace(0, 1, n_theta), np.linspace(0, 1, n_phi))
+    theta = u2theta(u[0]).flatten()
+    phi = u2phi(u[1]).flatten()
 
     # Create quadrature nodes (Spherical -> Cartesian coordinates)
-    sin_theta_m, cos_theta_m = (np.sin(theta_m), np.cos(theta_m))
-    sin_phi_m, cos_phi_m = (np.sin(phi_m), np.cos(phi_m))
+    sin_theta, cos_theta = (np.sin(theta), np.cos(theta))
+    sin_phi, cos_phi = (np.sin(phi), np.cos(phi))
 
-    omega_m = np.column_stack((cos_phi_m * sin_theta_m,
-                               sin_phi_m * sin_theta_m,
-                               cos_theta_m))
-    return theta_m, phi_m, omega_m
+    omega = np.column_stack((cos_phi * sin_theta,
+                             sin_phi * sin_theta,
+                             cos_theta))
+    return theta, phi, omega

test_brdf.py
@@ -1,16 +1,14 @@
 import pytest
 
 import numpy as np
-from visualize import read_tensor
-from brdf import (grid_sample, projected_area,
-                  sphere_surface_patch, visible_ndf, build_kernel,
-                  power_iteration, vndf_intp2sample,
-                  ndf_intp2sample, normalize_2D, normalize_4D)
+from brdf import (EPSILON, grid_sample, projected_area, sphere_surface_patch,
+                  visible_ndf, build_ndf_kernel, build_slope_kernel,
+                  power_iteration, vndf_intp2sample, ndf_intp2sample,
+                  normalize_slopes, normalize_2D, normalize_4D)
 
 import mitsuba
 # Set the any mitsuba variant
 mitsuba.set_variant('gpu_spectral')
-
 
 @pytest.mark.parametrize("n_theta", [128, 64])
 @pytest.mark.parametrize("n_phi", [1, 64])
@@ -55,17 +53,18 @@ def test03_sphere_integration(n_theta, n_phi, isotropic):
     assert((error < 1e-7).all())
 
 
-@pytest.mark.parametrize("filename", ["bin/spectralon_spec.bsdf"])
+@pytest.mark.parametrize("filename", ["bin/spectralon.pickle"])
 def test04_sigma_isotropic(filename):
-    # Read a tensor file from disk
-    tensor = read_tensor(filename)
+    # Read raw measurement data from disk
+    import pickle
+    data = pickle.load(open(filename, "rb"))
 
     # Create input values
-    isotropic = True
+    isotropic = data['isotropic']
 
     # Get NDF and sigma values
-    D = tensor['ndf']
-    sigma = tensor['sigma']
+    D = data['ndf_interp']
+    sigma = data['sigma']
 
     sigma_c = projected_area(D, isotropic)
 
@@ -110,8 +109,8 @@ def test06_vndf_sampler(filename):
 
 
 """
-TODO:   Fix error in NDF calculation.
-        NDF calculation not exact yet!
+TODO:   Fix error in NDF/slope calculation.
+        NDF/slope calculation not exact yet!
         That's why maximum error is currently still large.
 """
 @pytest.mark.parametrize("filename", ["bin/spectralon.pickle"])
@@ -129,34 +128,92 @@ def test07_ndf_isotropic(filename):
     frC = data['retro_data']
 
     # Get measurement values
-    D_intp = data['ndf_interp']
+    D = data['ndf_interp']
 
     if isotropic:
         n_phi = 125
     # Build kernel matrix
-    K = build_kernel(frC, n_theta, n_phi, isotropic)
+    K = build_ndf_kernel(frC, n_theta, n_phi, isotropic)
 
     # Compute NDF corresponding to first eigenvector
-    _, D_intp_c = power_iteration(K, 4)
+    _, D_c = power_iteration(K, 4)
 
     # Reshape to NDF format
     if isotropic:
-        D_intp_c = np.vstack((D_intp_c, D_intp_c))  # Stack NDF slice for phi=0
+        D_c = np.vstack((D_c, D_c))  # Stack NDF slice for phi=0
     else:
-        D_intp_c = np.reshape(D_intp_c, (n_phi, n_theta))
+        D_c = np.reshape(D_c, (n_phi, n_theta))
 
-    # Normalize
-    D_intp = normalize_2D(D_intp)
-    D_intp_c = normalize_2D(D_intp_c)
+    # Cosine-weight NDF
+    D_cw = np.zeros(D_c.shape)
+    theta = np.power(np.linspace(0, 1, n_theta), 2) * (np.pi / 2)
+    cos_theta = np.cos(theta)
+    for i in range(n_theta):
+        D_cw[:, i] = D_c[:, i] * cos_theta[i]
+    # Normalize weighted NDF or slopes
+    D_cw = normalize_2D(D_cw)
+    # Reverse weight NDF
+    D_c = np.zeros(D_c.shape)
+    for i in range(n_theta):
+        if cos_theta[i] > np.power(EPSILON, 4):
+            D_c[:, i] = D_cw[:, i] / cos_theta[i]
 
-    error = np.abs(D_intp - D_intp_c)
+    error = np.abs(D - D_c)
+    #print(np.max(error))
 
-    assert(D_intp.shape == D_intp_c.shape)
+    assert(D.shape == D_c.shape)
     assert((error < 1e-2).all())
 
 
 @pytest.mark.parametrize("filename", ["bin/spectralon.pickle"])
-def test08_vndf_interpolate(filename):
+def test08_slopes_isotropic(filename):
+    # Read raw measurement data from disk
+    import pickle
+    data = pickle.load(open(filename, "rb"))
+
+    isotropic = data['isotropic']
+
+    # Get dimensions of BRDF measurements
+    n_theta = data['retro_theta_res']
+    n_phi = data['retro_phi_res']
+
+    frC = data['retro_data']
+
+    # Get measurement values
+    D = data['ndf_interp']
+
+    if isotropic:
+        n_phi = 125
+    # Build kernel matrix
+    K = build_slope_kernel(frC, n_theta, n_phi, isotropic)
+
+    # Compute NDF corresponding to first eigenvector
+    _, P_c = power_iteration(K, 4)
+
+    # Reshape to NDF format
+    if isotropic:
+        P_c = np.vstack((P_c, P_c))  # Stack NDF slice for phi=0
+    else:
+        P_c = np.reshape(P_c, (n_phi, n_theta))
+
+    # Normalize
+    P_c = normalize_slopes(P_c, isotropic)
+    
+    # Get slopes from NDF
+    P = np.zeros(D.shape)
+    theta = np.power(np.linspace(0, 1, n_theta), 2) * (np.pi / 2)
+    cos_theta = np.cos(theta)
+    for i in range(n_theta):
+        P[:, i] = D[:, i] * np.power(cos_theta[i], 4)
+
+    error = np.abs(P - P_c)
+    #print(np.max(error))
+
+    assert(P.shape == P_c.shape)
+    assert((error < 1e-2).all())
+
+@pytest.mark.parametrize("filename", ["bin/spectralon.pickle"])
+def test09_vndf_interpolate(filename):
     # Read raw measurement data from disk
     import pickle
     data = pickle.load(open(filename, "rb"))
@@ -164,20 +221,21 @@ def test08_vndf_interpolate(filename):
     isotropic = data['isotropic']
 
     # Get measurement values
-    D_intp = data['ndf_interp']
+    D = data['ndf_interp']
     sigma = data['sigma']
     theta_i = data['vndf_theta_i']
     phi_i = data['vndf_phi_i']
-    Dvis_intp = data['vndf_interp']
+    Dvis = data['vndf_interp']
 
     # Calculate VNDF
-    Dvis_intp_c = visible_ndf(D_intp, sigma, theta_i, phi_i, isotropic)
+    Dvis_c = visible_ndf(D, sigma, theta_i, phi_i, isotropic)
 
     # Normalize
-    Dvis_intp = normalize_4D(Dvis_intp, theta_i, phi_i)
-    Dvis_intp_c = normalize_4D(Dvis_intp_c, theta_i, phi_i)
+    Dvis = normalize_4D(Dvis, theta_i, phi_i)
+    Dvis_c = normalize_4D(Dvis_c, theta_i, phi_i)
 
-    error = np.abs(np.nan_to_num(Dvis_intp) - np.nan_to_num(Dvis_intp_c))
+    error = np.abs(np.nan_to_num(Dvis) - np.nan_to_num(Dvis_c))
+    #print(np.max(error))
 
-    assert(Dvis_intp.shape == Dvis_intp_c.shape)
+    assert(Dvis.shape == Dvis_c.shape)
     assert((error < 1e-4).all())