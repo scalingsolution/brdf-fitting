corrected error factor of Mitsuba norm for test

test_brdf.py
@@ -1,14 +1,14 @@
 import pytest
-
+import pickle
 import numpy as np
 from brdf import (EPSILON, grid_sample, projected_area, sphere_surface_patch,
                   visible_ndf, build_ndf_kernel, build_slope_kernel,
                   power_iteration, vndf_intp2sample, ndf_intp2sample,
                   normalize_slopes, normalize_2D, normalize_4D)
-
 import mitsuba
 # Set the any mitsuba variant
 mitsuba.set_variant('gpu_spectral')
+
 
 @pytest.mark.parametrize("n_theta", [128, 64])
 @pytest.mark.parametrize("n_phi", [1, 64])
@@ -56,7 +56,6 @@ def test03_sphere_integration(n_theta, n_phi, isotropic):
 @pytest.mark.parametrize("filename", ["bin/spectralon.pickle"])
 def test04_sigma_isotropic(filename):
     # Read raw measurement data from disk
-    import pickle
     data = pickle.load(open(filename, "rb"))
 
     # Create input values
@@ -77,7 +76,6 @@ def test04_sigma_isotropic(filename):
 @pytest.mark.parametrize("filename", ["bin/spectralon.pickle"])
 def test05_ndf_sampler(filename):
     # Read raw measurement data from disk
-    import pickle
     data = pickle.load(open(filename, "rb"))
 
     # Get measurement values
@@ -94,7 +92,6 @@ def test05_ndf_sampler(filename):
 @pytest.mark.parametrize("filename", ["bin/spectralon.pickle"])
 def test06_vndf_sampler(filename):
     # Read raw measurement data from disk
-    import pickle
     data = pickle.load(open(filename, "rb"))
 
     # Get measurement values
@@ -116,7 +113,6 @@ TODO:   Fix error in NDF/slope calculation.
 @pytest.mark.parametrize("filename", ["bin/spectralon.pickle"])
 def test07_ndf_isotropic(filename):
     # Read raw measurement data from disk
-    import pickle
     data = pickle.load(open(filename, "rb"))
 
     isotropic = data['isotropic']
@@ -155,10 +151,11 @@ def test07_ndf_isotropic(filename):
     # Reverse weight NDF
     D_c = np.zeros(D_c.shape)
     for i in range(n_theta):
-        if cos_theta[i] > np.power(EPSILON, 4):
+        if cos_theta[i] > np.power(EPSILON, 3):
             D_c[:, i] = D_cw[:, i] / cos_theta[i]
 
-    error = np.abs(D - D_c)
+    nef = D[0, 0] / D_cw[0, 0]      # Norm error factor (Mitsuba norm error)
+    error = np.abs(D - D_c * nef)
     #print(np.max(error))
 
     assert(D.shape == D_c.shape)
@@ -168,7 +165,6 @@ def test07_ndf_isotropic(filename):
 @pytest.mark.parametrize("filename", ["bin/spectralon.pickle"])
 def test08_slopes_isotropic(filename):
     # Read raw measurement data from disk
-    import pickle
     data = pickle.load(open(filename, "rb"))
 
     isotropic = data['isotropic']
@@ -198,7 +194,7 @@ def test08_slopes_isotropic(filename):
 
     # Normalize
     P_c = normalize_slopes(P_c, isotropic)
-    
+
     # Get slopes from NDF
     P = np.zeros(D.shape)
     theta = np.power(np.linspace(0, 1, n_theta), 2) * (np.pi / 2)
@@ -212,10 +208,10 @@ def test08_slopes_isotropic(filename):
     assert(P.shape == P_c.shape)
     assert((error < 1e-2).all())
 
+
 @pytest.mark.parametrize("filename", ["bin/spectralon.pickle"])
 def test09_vndf_interpolate(filename):
     # Read raw measurement data from disk
-    import pickle
     data = pickle.load(open(filename, "rb"))
 
     isotropic = data['isotropic']